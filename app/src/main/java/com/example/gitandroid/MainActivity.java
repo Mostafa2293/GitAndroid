b0fbb340ae0df20a82b42d8331b1544710e2fbb7
Mostafa2293 <mostafamohamed05@gmail.com>
2019-12-04 11:06:39 +0200

    this is printing statment

--- app/src/main/java/com/example/gitandroid/MainActivity.java
+++ app/src/main/java/com/example/gitandroid/MainActivity.java
@@ -14,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("Mytag","Msg1");
         Log.i("Mytag","Msg2");
-        Log.i("Mytag","Msg3");
+
+        System.out.println("Hello");
 
 
     }
