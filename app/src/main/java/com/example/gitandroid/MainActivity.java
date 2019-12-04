d7612f05576960459978722017c6b108543aca79
Mostafa2293 <mostafamohamed05@gmail.com>
2019-12-04 11:51:05 +0200

    this is the first animated text view

--- app/src/main/java/com/example/gitandroid/MainActivity.java
+++ app/src/main/java/com/example/gitandroid/MainActivity.java
@@ -4,23 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("Mytag","Msg1");
-        Log.i("Mytag","Msg2");
+        txtHelloWorld = findViewById(R.id.txtHelloWorld);
 
-        System.out.println("Hello");
+        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtHelloWorld.animate().alpha(0.1f).setDuration(3000);
+            }
+        });
 
 
     }
 
-    private void test() {
-        Log.i("Method","this is the new branch MethodOne !!");
-    }
+
 }
